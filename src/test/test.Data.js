f076d5db1463345919b2e2221b2638e7c384c33d
dev-ascript <dev.ascript@gmail.com>
2015-07-23 01:46:00 +0900

    추가 작업

--- src/test/test.Data.js
+++ src/test/test.Data.js
@@ -30,7 +30,6 @@ window.testUtils = {
             }
 
             if (attrValue !== val) {
-                console.log(attrValue);
                 if (message === undefined) {
                     message = [
                         '<strong>',
@@ -56,6 +55,74 @@ window.testUtils = {
             }
 
         });
+
+    },
+
+    checkText: function(name, $target, data, message) {
+
+        var val = $target.html().toLowerCase().
+            replace(/&lt;/g, '<').
+            replace(/&gt;/g, '>').
+            replace(/&amp;/g, '&').
+            replace(/<br[^>]*>/gi, '').
+            replace(/\n/g, '');
+        data = data.toLowerCase().
+            replace(/<br[^>]*>/gi, '').
+            replace(/\n/g, '');
+
+        if (val !== data) {
+            if (message === undefined) {
+                message = [
+                    '<strong>',
+                    $target[0].nodeName,
+                    '</strong>의 <strong>',
+                    '</strong> 텍스트 값이 <strong>',
+                    val,
+                    '</strong> 입니다.<br />&gt;&gt;&gt; <strong>',
+                    data,
+                    '</strong>'
+                ].join('');
+            }
+
+            testResult.errors.push('[' + name + '] : ' + message);
+            $target.addClass('errorElement');
+        }
+
+    },
+
+    checkHtml: function(name, $target, data, message) {
+
+        var val = $target.html().toLowerCase().
+            replace(/&lt;/g, '<').
+            replace(/&gt;/g, '>').
+            replace(/&amp;/g, '&').
+            replace(/<br[^>]*>/gi, '').
+            replace(/<[^>]*$/, '').
+            replace(/\n/g, '');
+        data = data.toLowerCase().
+            replace(/<br[^>]*>/gi, '').
+            replace(/<[^>]*$/, '').
+            replace(/\n/g, '');
+
+        if (val !== data) {
+            console.log(val);
+            console.log(data);
+            if (message === undefined) {
+                message = [
+                    '<strong>',
+                    $target[0].nodeName,
+                    '</strong>의 <strong>',
+                    '</strong> html 값이 <strong>',
+                    val,
+                    '</strong> 입니다.<br />&gt;&gt;&gt; <strong>',
+                    data,
+                    '</strong>'
+                ].join('');
+            }
+
+            testResult.errors.push('[' + name + '] : ' + message);
+            $target.addClass('errorElement');
+        }
 
     }
 
@@ -89,6 +156,50 @@ window.testData = {
             });
         }
 
+    },
+
+
+    "_variable-text": {
+        test: function(section) {
+
+            var options = section.options;
+            var $pre = $(options.tag);
+            var idx = 0;
+            var dt = json.package;
+            var arr = [
+                'description',
+                'spacialChar',
+                'htmlStr',
+                'project'
+            ];
+
+            TagWire.each(arr, function(s, i) {
+                var $tar = $pre.find('._' + s + '-text');
+                var val = String(dt[s]);
+                testUtils.checkText(options.titleValue, $tar, val);
+            });
+
+        }
+    },
+
+
+    "_variable-html": {
+        test: function(section) {
+            var options = section.options;
+            var $pre = $(options.tag);
+            var idx = 0;
+            var dt = json.package;
+            var arr = [
+                'htmlStr',
+                'spacialChar'
+            ];
+
+            TagWire.each(arr, function(s, i) {
+                var $tar = $pre.find('._' + s + '-html');
+                var val = String(dt[s]);
+                testUtils.checkHtml(options.titleValue, $tar, val);
+            });
+        }
     }
 
 };
